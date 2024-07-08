51634bef9934d1a0289dce3d896d49525459ddbb
Jack <pyshkin7979@gmail.com>
2024-07-08 13:35:35 +0300

    task

--- 1. minutesToHours/script.js
+++
@@ -1,8 +0,0 @@
-// Write a function named minutesToHours that receives a number of minutes as parameter and returns a number representing the same amount of time but in hours.
-
-let minutes = 150;
-function minutesToHours(minutes) {
-    return minutes/60;
-}
-
-export { minutesToHours };
